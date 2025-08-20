3f1b946c344c1ca1bd11e9adda5899f96097780d
Raj Srivastava <raj113192007@gmail.com>
2025-08-20 16:54:58 +0530

    hd

---
+++ area_of_rect.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int l, b ;
+    cout<<"Enter length: ";
+    cin>> l ;
+    cout<<"Enter Breadth";
+    cin>> b ;
+    cout << l*b;
+    cout << "Thank You";
+    return 0;
+}
+//This code prints area of rectangle
---
+++ sum_two_no.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x;
+    int y;
+    cout << "enter first number : \n";
+    cin >> x;
+    cout<< "enter second number: \n";
+    cin >> y;
+    cout<< x + y;
+    
+    return 0;
+}
