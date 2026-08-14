7868a516ba9db2e298004f15b1f4f633783c99a0
raj-113192007 <raj113192007@gmail.com>
2026-08-14 02:18:22 -0400

    endl concept

---
+++ endl.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main (){
+    cout<< "hi guys" << endl << "this was a test ";//concept of endl
+    return 0;
+}
--- typecast.cpp
+++ typecast.cpp
@@ -7,5 +7,12 @@ int main () {
     float y = (float)x;
     cout << x / 2;
 
+    int t;
+    int g;
+    int h;
+    cout << "enter the value of the second variable y";
+    cin >> y;
+    char d = (char)t;
+    char u = (char)h;//determine the new value of the g 
     return 0;
 }
